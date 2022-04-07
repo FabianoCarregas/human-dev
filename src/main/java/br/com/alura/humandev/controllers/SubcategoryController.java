4c3da9e47b8462903f41bf9e7deaacfcd215af70
FabianoCarregas <fabianofac_@hotmail.com>
2022-04-07 16:10:29 -0300

    starting week 9

---
+++ src/main/java/br/com/alura/humandev/controllers/SubcategoryController.java
@@ -0,0 +1,10 @@
+package br.com.alura.humandev.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SubcategoryController {
+
+
+
+}
---
+++ src/main/java/br/com/alura/humandev/repositories/SubcategoryRepository.java
@@ -0,0 +1,4 @@
+package br.com.alura.humandev.repositories;
+
+public class SubcategoryRepository {
+}
