000ab74ede352086d0d9f9f2374fa7511dcc7504
FabianoCarregas <fabianofac_@hotmail.com>
2022-05-11 15:46:22 -0300

    lombok

--- src/main/java/br/com/alura/humandev/dtos/LoginForm.java
+++ src/main/java/br/com/alura/humandev/dtos/LoginForm.java
@@ -2,11 +2,9 @@ package br.com.alura.humandev.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class LoginForm {
 
     private String email;
