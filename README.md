884eeadd8020f1cdc342671507120cf69e68cbfd
Fabs <fabs@pop-os.localdomain>
2023-02-06 15:12:37 -0300

    creating a new readme and dump file

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A Java jsp plataform that create an EduTech environment, on that is possible to 
 create, edit and remove; categories, subcategories and courses matters. you can also login as an student where you can check the categories with courses, 
 and your information.
 
-## to run the project follow the steps bellow
+## requirement to run the project, follow the steps below
 
 HumanDev it's using by java 17, so the first step that you take is verify if you have a java 17 or superior in your computer
 you can check in your terminal by the command below
@@ -70,11 +70,34 @@ now you can try again, just leave the password empty and press enter
 mysql -u root -p
 ```
 
+- Populating the database 
+
+You can simply populate the database by using the dump file saved in /resources/dump-DB.sql,
+run the project once to check if it has no errors, if all runs fine you'll have a new database
+named humandev.
+
+You'll be able to populate the database by using the command below, replacing "project-directory" with no quotes, to the directory 
+where you had been cloned the project before.
+```sudo mysql -u root- p humandev < "project-directory"/human-dev/src/main/resources/dump-DB.sql```
+
+## Login
+
+With the project up, go to ```http://localhost:8080/login``` in your web-browser
+
+We've got 2 roles, 'adm' and 'student'
+to log-in as an admin use:
+
+user: ```admin@alura.com```
+password: ```12345```
+
 ![alt text](./src/main/resources/assets/landingPage.png)
+
+to access the admin panel log-in as an admin and go to ```http://localhost:8080/admin/categories```
+
+here you can see all categories, edit or deactivate then or go to 'Subcategories' & 'Courses'
 ![alt text](./src/main/resources/assets/admCategories.png)
-![alt text](./src/main/resources/assets/knowMore.png)
 ![alt text](./src/main/resources/assets/admEditCategories.png)
 
-Once everything is set up, in the running interface, you can fill in the url with the 'machine'
-your email and your password, choose the environment that you are deploying, and push the START button
-and wait for the bot do his thing.
+Back to mainPage ```http://localhost:8080/login``` on the right side, you can see all categories with more than one course and their information.
+![alt text](./src/main/resources/assets/landingPageRight.png)
+![alt text](./src/main/resources/assets/knowMore.png)
--- src/main/resources/dump-DB.sql
+++ src/main/resources/dump-DB.sql
@@ -1,34 +1,39 @@
-INSERT INTO humandev.category (id, active, category_description, code, hexa_color, icon, name, ordination, study_guide) VALUES (1, true, 'Programe nas principais linguagens e plataformas. Iniciantes são bem vindos nos cursos de lógica e JavaScript.', 'programacao', '#00c86f', 'https://www.alura.com.br/assets/api/formacoes/categorias/512/inovacao-gestao-transparent.png', 'Programação', 1, 'study guide');
-INSERT INTO humandev.category (id, active, category_description, code, hexa_color, icon, name, ordination, study_guide) VALUES (2, true, 'Aprenda Git. Entenda a entrega contínua. Estude Linux. Gerencie servidores na nuvem. Explore o mundo de Internet das coisas e da robótica.', 'devops', '#f16165', 'https://www.alura.com.br/assets/api/formacoes/categorias/512/devops-transparent.png', 'DevOps', 2, 'study guide');
-INSERT INTO humandev.category (id, active, category_description, code, hexa_color, icon, name, ordination, study_guide) VALUES (3, true, 'business', 'business', '#ff8c2a', 'https://www.alura.com.br/assets/api/formacoes/categorias/512/inovacao-gestao-transparent.png', 'Business', 5, '');
-INSERT INTO humandev.category (id, active, category_description, code, hexa_color, icon, name, ordination, study_guide) VALUES (4, true, 'science', 'science', '#FFF', 'https://www.alura.com.br/assets/api/formacoes/categorias/128/data-science.png', 'Science', 6, 'muitos estudos');
-INSERT INTO humandev.category (id, active, category_description, code, hexa_color, icon, name, ordination, study_guide) VALUES (5, true, 'muito html e css', 'front', '#FFA', 'https://www.alura.com.br/assets/api/formacoes/categorias/128/front-end.png', 'FrontEnd', 11, 'estudos');
+INSERT INTO humandev.profile (name) VALUES
+    ('ROLE_STUDENT'),
+    ('ROLE_ADMIN');
 
-INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES (1, true, 'java', 'Java', 1, 'guia', null, 1);
-INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES (2, true, 'java-e-persistencia', 'Java e Persistência', 3, 'guia', null, 1);
-INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES (3, true, 'php', 'PHP', 2, 'guia', null, 1);
-INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES (4, true, 'cobol', 'COBOL', 4, 'muitos estudos', null, 1);
-INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES (5, true, 'builds', 'Builds e Controle de versão', 5, 'estudos', null, 2);
-INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES (6, true, 'soft', 'SoftSkills', 6, 'aleatório', null, 2);
-INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES (7, true, 'a', 'castegoriateste123', 6, 'olele', null, 4);
-INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES (8, true, 'p', 'Program', 2, 'olar', null, 4);
+INSERT INTO humandev.user (email, password) VALUES
+    ('fabiano@alura', '$2a$10$esVY9A3zjete7yeNyw0fNeq0iuwd9l5B2kVLNL9.6VHXMH9e3qqb2'),
+    ('admin@alura.com', '$2a$10$esVY9A3zjete7yeNyw0fNeq0iuwd9l5B2kVLNL9.6VHXMH9e3qqb2');
 
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (4, true, 'git-e-github-para', 'muita informação', 6, null, 'tio Bill', 'Git e Github ', 'developers', 1);
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (5, true, 'java-jpa-consultas-avancadas-performance-modelos-complexos', 'muita informação', 16, null, 'tio Bill', 'Java e JPA: Consultas avançadas performance e modelos complexos', 'developers', 1);
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (6, true, 'java-introducao-orientacao-objetos', 'muita informação', 8, null, 'tio Bill', 'Java OO: Introdução à Orientação a Objetos', 'Desenvolvedores que estão começando com Java e querem aprender mais sobre OO.', 1);
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (7, true, 'java-primeiros-passos', 'muita informação', 8, null, 'tio Bill', 'Java JRE e JDK: Escreva o seu primeiro código com Eclipse', 'Desenvolvedores que estão começando com Java e querem aprender mais sobre OO.', 1);
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (8, true, 'angular', '- Conheça todos os comando da CLI do Angular. <br> - Saiba como funcionam as Workspaces no angular.json. <br> - Saiba como o Schematics pode aumentar sua produtividade <br> - Garanta consistência e otimizações para produção com o Builder.', 15, null, 'tio Bill', 'Angular CLI', 'developers', 2);
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (9, true, 'gam', '- Conheça todos os comando da CLI do Angular. <br> - Saiba como funcionam as Workspaces no angular.json. <br> - Saiba como o Schematics pode aumentar sua produtividade <br> - Garanta consistência e otimizações para produção com o Builder.', 15, null, 'Silveirinha', 'Projecting games', 'developers', 6);
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (10, true, 'softskils', 'muito soft', 19, null, 'Maria', 'SoftSkills', 'Pessoas', 1);
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (11, true, 'novo', 'muito soft', 4, null, 'Maria', 'novo curso', 'developers', 6);
-INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES (12, true, 'test', 'muita informação', 1, null, 'tio Bill', 'testando123', '1', 1);
+INSERT INTO humandev.profile_users (profiles_id, users_id) VALUES
+    (1, 1),
+    (2, 2);
 
-INSERT INTO humandev.profile (id, name) VALUES (1, 'ROLE_STUDENT');
-INSERT INTO humandev.profile (id, name) VALUES (2, 'ROLE_ADMIN');
+INSERT INTO humandev.category (id, active, category_description, code, hexa_color, icon, name, ordination, study_guide) VALUES
+    (1, true, 'Programe nas principais linguagens e plataformas. Iniciantes são bem vindos nos cursos de lógica e JavaScript.', 'programacao', '#00c86f', 'https://www.alura.com.br/assets/api/formacoes/categorias/512/inovacao-gestao-transparent.png', 'Programação', 1, 'study guide'),
+    (2, true, 'Aprenda Git. Entenda a entrega contínua. Estude Linux. Gerencie servidores na nuvem. Explore o mundo de Internet das coisas e da robótica.', 'devops', '#f16165', 'https://www.alura.com.br/assets/api/formacoes/categorias/512/devops-transparent.png', 'DevOps', 2, 'study guide'),
+    (3, true, 'business', 'business', '#ff8c2a', 'https://www.alura.com.br/assets/api/formacoes/categorias/512/inovacao-gestao-transparent.png', 'Business', 5, ''),
+    (4, true, 'science', 'science', '#FFF', 'https://www.alura.com.br/assets/api/formacoes/categorias/128/data-science.png', 'Science', 6, 'muitos estudos'),
+    (5, true, 'html e css', 'front', '#FFA', 'https://www.alura.com.br/assets/api/formacoes/categorias/128/front-end.png', 'FrontEnd', 11, 'estudos');
 
-INSERT INTO humandev.user (id, email, password) VALUES (1, 'fabiano@alura', '$2a$10$esVY9A3zjete7yeNyw0fNeq0iuwd9l5B2kVLNL9.6VHXMH9e3qqb2');
-INSERT INTO humandev.user (id, email, password) VALUES (2, 'admin@alura.com', '$2a$10$esVY9A3zjete7yeNyw0fNeq0iuwd9l5B2kVLNL9.6VHXMH9e3qqb2');
+INSERT INTO humandev.subcategory (id, active, code, name, ordination, study_guide, subcategory_description, category_id) VALUES
+    (1, true, 'java', 'Java', 1, 'guia', null, 1),
+    (2, true, 'java-e-persistencia', 'Java e Persistência', 3, 'guia', null, 1),
+    (3, true, 'php', 'PHP', 2, 'guia', null, 1),
+    (4, true, 'cobol', 'COBOL', 4, 'muitos estudos', null, 2),
+    (5, true, 'builds', 'Builds e Controle de versão', 5, 'estudos', null, 2),
+    (6, true, 'soft', 'SoftSkills', 6, 'aleatório', null, 2),
+    (7, true, 'js', 'js', 6, 'js-begin', null, 3),
+    (8, true, 'c', 'c', 2, 'c-starting-up', null, 3);
 
-INSERT INTO humandev.profile_users (profiles_id, users_id) VALUES (1, 1);
-INSERT INTO humandev.profile_users (profiles_id, users_id) VALUES (2, 2);
-
+INSERT INTO humandev.course (id, active, code, course_description, course_time_hours, developed_skills, instructor, name, target_audience, subcategory_id) VALUES
+    (4, true, 'git-e-github-para', 'muita informação', 6, null, 'tio Bill', 'Git e Github ', 'developers', 1),
+    (5, true, 'java-jpa-consultas-avancadas-performance-modelos-complexos', 'muita informação', 16, null, 'tio Bill', 'Java e JPA: Consultas avançadas performance e modelos complexos', 'developers', 1),
+    (6, true, 'java-introducao-orientacao-objetos', 'muita informação', 8, null, 'tio Bill', 'Java OO: Introdução à Orientação a Objetos', 'Desenvolvedores que estão começando com Java e querem aprender mais sobre OO.', 4),
+    (7, true, 'java-primeiros-passos', 'muita informação', 8, null, 'tio Bill', 'Java JRE e JDK: Escreva o seu primeiro código com Eclipse', 'Desenvolvedores que estão começando com Java e querem aprender mais sobre OO.', 4),
+    (8, true, 'angular', '- Conheça todos os comando da CLI do Angular. <br> - Saiba como funcionam as Workspaces no angular.json. <br> - Saiba como o Schematics pode aumentar sua produtividade <br> - Garanta consistência e otimizações para produção com o Builder.', 15, null, 'tio Bill', 'Angular CLI', 'developers', 4),
+    (9, true, 'ux', 'consistência e otimizações para produção com o Builder.', 15, null, 'Silveirinha', 'Projecting games', 'developers', 7),
+    (10, true, 'logica', 'performance-modelos-complexos', 19, null, 'Maria', 'SoftSkills', 'Pessoas', 7),
+    (11, true, 'programacao', 'consultas-avancadas-performance-modelos', 4, null, 'Maria', 'Entenda a entrega contínua', 'developers', 7),
+    (12, true, 'test', 'Entenda a entrega contínua', 1, null, 'tio Bill', 'Entenda a entrega contínua', '1', 7);
